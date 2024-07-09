3e9a2daceac239d7fe5e59dd4cf6c413ea0f279a
STEVnhe <aahyanpanta390@gmail.com>
2024-07-09 14:13:59 +0545

    tt

---
+++ src/nepalicitizen.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class nepalicitizen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name of the perso");
+    }
+}
