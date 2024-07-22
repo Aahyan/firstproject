85c835658aff0636fcb31a00abcf771b3de44927
STEVnhe <aahyanpanta390@gmail.com>
2024-07-22 17:54:14 +0545

    tt

---
+++ src/hw3.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class hw3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter amount");
+        int amount = sc.nextInt();
+        int count = amount/1000;
+        if(count>0){
+            System.out.println("1000x");
+            amount %= 1000;
+        }
+        count = amount/500;
+        if (count>0){
+            System.out.println("500x");
+            amount %= 500;
+        }
+        count = amount/100;
+        if (count>0){
+            System.out.println("1000x");
+            amount %= 100;
+        }
+        count = amount/50;
+        if (count>0){
+            System.out.println("50x");
+            amount %= 50;
+        }
+        count = amount/10;
+        if (count>0){
+            System.out.println("10x");
+            amount %= 10;
+        }
+        count = amount/5;
+        if (count>0){
+            System.out.println("5x");
+        }
+    }
+}
---
+++ src/hw5.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class hw5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter amount");
+        int amount = sc.nextInt();
+        int count = amount/1000;
+        if(count>0){
+            System.out.println("1000x");
+            amount = amount/1000;
+        }
+        count = amount/500;
+        if(count>0){
+            System.out.println("500x");
+            amount = amount/500;
+        }
+        count = amount/100;
+        if(count>0){
+            System.out.println("100x");
+            amount = amount/100;
+        }
+        count = amount/50;
+        if(count>0){
+            System.out.println("50x");
+            amount = amount/50;
+        }
+        count = amount/10;
+        if(count>0){
+            System.out.println("10x");
+            amount = amount/10;
+        }
+        count = amount/5;
+        if(count>0){
+            System.out.println("5x");
+            amount = amount/5;
+        }
+    }
+}
